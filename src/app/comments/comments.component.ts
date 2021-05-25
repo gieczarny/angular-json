2450d69da229659241324d0fb0af054fe8e733b3
giedeve <gieczarny@gmail.com>
2021-05-26 01:36:17 +0200

    add component and change comments

---
+++ src/app/comments/comments.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import axios from 'axios';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.scss']
+})
+export class CommentsComponent implements OnInit {
+  post: {} = { "title": "Title is loading...", "post": "Text is loading..." };
+  id: number;
+  constructor(private route: ActivatedRoute, private router: Router) {
+  }
+
+  ngOnInit(): void {
+
+    axios
+      .get('http://localhost:3000/posts')
+      .then(response => {
+        if (this.router.url === '/comments') {
+          this.post = response.data.slice().reverse()[0];
+
+        } else {
+          this.id = Number(this.route.snapshot.paramMap.get('id'));
+          this.post = response.data.slice()[this.id - 1];
+        }
+      }
+      );
+  }
+  everythingPosts = () => {
+    this.router.navigate(['/']);
+  }
+}
