d0dfb5ed7364a392abb6b5da92f33f5b09e22403
giedeve <gieczarny@gmail.com>
2021-05-26 01:33:03 +0200

    add and change add-post

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import axios from 'axios';
+
 
 @Component({
   selector: 'app-add-post',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
   }
+
+  onAddItem(form: NgForm) {
+      axios
+        .post('http://localhost:3000/posts', {
+          "title": form.value.title, "post": form.value.postText
+        });
+      this.router.navigate(['/comments']);
+    }
 
 }
