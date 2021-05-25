8a543d96b8ad60c56e1a134c7377eb3a689ee626
giedeve <gieczarny@gmail.com>
2021-05-26 01:34:35 +0200

    add and change post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  @Input('postElement') element;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  confirmPostRemoval = () => {
+    console.log(document.getElementsByClassName("myPopup"));
+    let popups = document.getElementsByClassName("myPopup");
+    let popup = Array.prototype.slice.call(popups, 0).reverse()[this.element.id - 1];
+    popup.classList.toggle("show");
+    return true;
+  }
+
+  deletePost = () => {
+    if (this.confirmPostRemoval()) {
+      axios({
+        method: 'DELETE',
+        url: 'http://localhost:3000/posts/' + (this.element.id),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (this.router.url === '/posts') {
+        window.location.reload()
+      } else {
+        this.router.navigate(['/posts']);
+      }
+    }
+  }
+
+  editPost = () => {
+    this.router.navigate(['/edit/' + this.element.id]);
+  }
 }
